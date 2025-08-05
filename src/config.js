fb91e5f00da8dfc78dda8f9a2ff3cbc7e149e633
srinath <iamsrinath5255@gmail.com>
2025-08-05 16:47:58 +0530

    google login

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 // config.js
-// const BASE_URL="https://blogsite-backend.vercel.app/api"
-const BASE_URL="http://localhost:3005/api"
+const BASE_URL="https://blogsite-backend.vercel.app/api"
+// const BASE_URL="http://localhost:3005/api"
 export default BASE_URL;
